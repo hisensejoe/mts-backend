feece5a8703364c0f965ccdd884d8faf9eac47c4
Nana Kojo Ewusie <enanakojo@icloud.com>
2026-03-08 21:14:32 +0000

    update config

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 python -m app.scripts.wait_for_db
 alembic upgrade head
 
-exec uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+exec uvicorn main:app --host 0.0.0.0 --port 8000
